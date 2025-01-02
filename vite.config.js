38bb3d4a1140303986006ddd19764af26036ed2e
서민정 <ybsm0629@gmail.com>
2025-01-02 14:46:44 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // GitHub Pages에 맞춰 설정
+  build: {
+    outDir: 'public' // 빌드 결과물을 public 폴더에 저장
+  }
+});
